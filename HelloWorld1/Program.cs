6a1d7d3a39dae3b6bed4ab5ca6c147c1ddefe8b6
USER <USER@DESKTOP-IJ48L0U>
2024-07-30 11:47:30 +0700

    Add project files.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Selamat datang di kelas 10 Game");
+            string nama = "Abdillah";
+            Console.WriteLine("Hallo " + nama + " apa kabar?");
+            Console.WriteLine();
+            string Nama;
+            Console.WriteLine("Masukan nama : ");
+            Nama = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hallo " + Nama + " Selamat datang di C#!");
+            */
+
+            string nama, asal;
+            Console.WriteLine("Masukan nama : ");
+            nama = Console.ReadLine();
+            Console.WriteLine("\nMasukan asal : ");
+            asal = Console.ReadLine();
+         
+
+            Console.WriteLine("\nHallo {0} dari {1} selamat datang di C#",nama,asal);
+        }
+    }
+}
